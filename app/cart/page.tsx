5e4733c67ae97710ce91c704d2d574f7f42cb9c6
marion586 <marionmenye@gmail.com>
2025-05-03 12:25:59 +0300

    created cart item

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Container from "../component/Container";
+import CartClient from "./CartClient";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="pt-8">
+      <Container>
+        <CartClient />
+      </Container>
+    </div>
+  );
 };
 
 export default Cart;
