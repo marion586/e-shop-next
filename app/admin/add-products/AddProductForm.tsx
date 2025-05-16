de148b93c7de6e68e140f048d612792589d67946
marion586 <marionmenye@gmail.com>
2025-05-16 14:01:29 +0300

    add product to database

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,22 +1,34 @@
 "use client";
 
+import Button from "@/app/component/Button";
 import Heading from "@/app/component/Heading";
 import CategoryInput from "@/app/component/inputs/CategoryInput";
 import CustomCheckBox from "@/app/component/inputs/CustomCheckBox";
 import Input from "@/app/component/inputs/Input";
 import SelectColor from "@/app/component/inputs/SelectColor";
 import TextArea from "@/app/component/inputs/TextArea";
-import { ImageType } from "@/types";
+import firebaseApp from "@/libs/firebase";
+import { ImageType, UploadedImageType } from "@/types";
 import { categories } from "@/utils/categories";
 import { colors } from "@/utils/Colors";
 import React, { useCallback, useEffect, useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { resolve } from "path";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const AddProductForm = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [images, setImages] = useState<ImageType[] | null>(null);
   const [isProductCreated, setIsProductCreated] = useState<Boolean>(false);
-
+  const router = useRouter();
   console.log("images====>>", images);
 
   const {
@@ -79,6 +91,101 @@ const AddProductForm = () => {
     });
   }, []);
 
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data, "product data");
+    setIsLoading(true);
+    let uploadedImages: UploadedImageType[] = [];
+
+    if (!data.category) {
+      setIsLoading(false);
+      return toast.error("Category is not selected");
+    }
+    if (!data.images || data.images.length == 0) {
+      setIsLoading(false);
+      return toast.error("no selected images");
+    }
+    const handleImageUploads = async () => {
+      toast("Creating product . please Wait..");
+
+      try {
+        for (const item of data.images) {
+          if (item.image) {
+            const fileName = new Date().getTime() + "-" + item.image.name;
+            const storage = getStorage(firebaseApp);
+            const storageRef = ref(storage, `products/${fileName}`);
+
+            const uploadTask = uploadBytesResumable(storageRef, item.image);
+
+            await new Promise<void>((resolve, reject) => {
+              uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                  // Observe state change events such as progress, pause, and resume
+                  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                  const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                  console.log("Upload is " + progress + "% done");
+                  switch (snapshot.state) {
+                    case "paused":
+                      console.log("Upload is paused");
+                      break;
+                    case "running":
+                      console.log("Upload is running");
+                      break;
+                  }
+                },
+                (error) => {
+                  console.log("error uploading image", error);
+                  reject(error);
+                },
+                () => {
+                  // Handle successful uploads on complete
+                  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                  getDownloadURL(uploadTask.snapshot.ref)
+                    .then((downloadURL) => {
+                      uploadedImages.push({
+                        ...item,
+                        image: downloadURL,
+                      });
+                      console.log("File available at", downloadURL);
+                      resolve();
+                    })
+                    .catch((error) => {
+                      console.log("Error getting the download URL", error);
+                      reject(error);
+                    });
+                }
+              );
+            });
+          }
+        }
+      } catch (error) {
+        setIsLoading(false);
+        console.log("Error handling image uploads", error);
+        return toast.error("Error handling image uploads");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    await handleImageUploads();
+    const productData = { ...data, images: uploadedImages };
+    console.log(productData);
+
+    axios
+      .post("/api/product", productData)
+      .then(() => {
+        toast.success("Product created");
+        router.refresh();
+      })
+      .catch((error) => {
+        toast.error("Something went wrong");
+        setIsLoading(false);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
   return (
     <>
       <Heading title="Add product" center />
@@ -173,6 +280,11 @@ const AddProductForm = () => {
           ))}
         </div>
       </div>
+
+      <Button
+        label={isLoading ? "Loading ..." : "Add Product"}
+        onClick={handleSubmit(onSubmit)}
+      />
     </>
   );
 };
